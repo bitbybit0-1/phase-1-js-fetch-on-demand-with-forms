09579eb262771337d518d9e0a25a2686dfe06244
Joseph Sutera <joesutera11@gmail.com>
2022-08-12 12:20:53 -0400

    commit tree bc40768597a33e990dbf2e832620a82c87cab8c2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 const init = () => {
-  
+const inputForm = document.querySelector('form');
+inputForm.addEventListener('submit',(event)=>{
+event.preventDefault();
+const input=document.querySelector(event.target.children[1].value)
+
+fetch(`${input}`)
+.then(response=>response.json())
+.then(data=>{
+    console.log(data)
+})
+})
+   
 }
 
 document.addEventListener('DOMContentLoaded', init);
